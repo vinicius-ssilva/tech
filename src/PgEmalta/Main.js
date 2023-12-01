235c7cb6142417648ad7a4dfed191a6d410add49
Vinicius <vinilopesilva1@unesc.net>
2023-12-01 11:26:50 -0300

    Main/Em alta

--- src/PgEmalta/Main.js
+++ src/PgEmalta/Main.js
@@ -1,7 +1,16 @@
 import styles from "../PgEmalta/Main.module.css"
+import img from "../PgEmalta/img/vr.jpeg"
+import img1 from "../PgEmalta/img/vrPersona.jpeg"
+import img2 from "../PgEmalta/img/novaLinhaGalaxyS23-Fe(1).jpeg"
+
+
 export default function Main(){
     return(
         <div className={styles.contentMain}>
+            <section>
+                ,,,,,,,,,,,,,,,,,,,,,
+            </section>
+ 
 
             </div>
     )
